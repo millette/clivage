9fc13deb9ef022e5f868622bbf6ed1ed26f3e1bb
Robin Millette <robin@millette.info>
2017-04-11 11:12:33 -0400

    Add failing test.

--- test.js
+++ test.js
@@ -1,7 +1,22 @@
 'use strict'
+
+// npm
+import joi from 'joi'
 import test from 'ava'
+
+// self
 import fn from './'
 
-test('title', async t => {
-  t.is(await fn('unicorns'), 'unicorns & rainbows')
+const schema = joi.object({
+  port: joi.number(),
+  more: joi.string(),
+  srv: joi.string().uri().default('http://localhost:5984').optional(),
+  user: joi.string().optional(),
+  pw: joi.string().optional()
+})
+  .with('pw', 'user')
+  .with('user', 'pw')
+
+test('title', t => {
+  t.is(fn({schema}), 'unicorns & rainbows')
 })
