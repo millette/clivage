0d1ea89a124ceebede4b9531f624f1188d2338e6
Robin Millette <robin@millette.info>
2017-04-12 00:29:02 -0400

    New test (supporting .env file).

---
+++ test/t1/test.js
@@ -0,0 +1,31 @@
+'use strict'
+
+// npm
+import joi from 'joi'
+import test from 'ava'
+
+// self
+import fn from '../../'
+
+const argv = ['--more=rap']
+
+const schema = joi.object({
+  badam: joi.boolean().truthy('').optional(),
+  port: joi.number(),
+  more: joi.string(),
+  srv: joi.string().uri().default('http://localhost:5984').optional(),
+  user: joi.string().optional(),
+  pw: joi.string().optional()
+})
+  .with('pw', 'user')
+  .with('user', 'pw')
+
+const envPath = [__dirname, '.env'].join('/')
+
+test('flags #2', t => {
+  const ret = fn({ schema, argv, envPath })
+  // console.log('FLAGS#2', ret.flags)
+  t.is(ret.flags.more, 'rap')
+  t.is(ret.flags.port, 666)
+  t.is(ret.flags.srv, 'http://localhost:5984')
+})
